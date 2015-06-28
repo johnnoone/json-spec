676bb3cc30a719132fde5a28b0564a474e0c98f8
Xavier Barbosa <clint.northwood@gmail.com>
2015-06-28 16:35:41 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='json-spec',
-    version='0.1',
+    version='0.9.9',
     description='Implements JSON Schema, JSON Pointer and JSON Reference.',
     long_description=content,
     author='Xavier Barbosa',
