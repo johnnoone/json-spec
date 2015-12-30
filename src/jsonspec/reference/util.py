4079f6445857b3c428650ca905aa6e9c9545ee40
Richard Downe <richard@casetext.com>
2015-12-29 18:15:00 -0800

    Made FilesystemProvider's traversal more robust.

--- src/jsonspec/reference/util.py
+++ src/jsonspec/reference/util.py
@@ -4,7 +4,7 @@
 
 """
 
-__all__ = ['ref', 'loop', 'Mapping', 'MutableMapping']
+__all__ = ['ref', 'Mapping', 'MutableMapping']
 
 import fnmatch
 import os
@@ -17,21 +17,6 @@ def ref(obj):
         return obj['$ref']
     except (KeyError, TypeError):
         return None
-
-
-def loop(directory, pattern='*'):
-    regex = fnmatch.translate(pattern)
-    matcher = re.compile(regex)
-
-    def _loop(directory):
-        for file in os.listdir(directory):
-            path = os.path.join(directory, file)
-            if os.path.isfile(path) and matcher.match(path):
-                yield path
-            elif os.path.isdir(path):
-                for subpath in loop(path):
-                    yield subpath
-    return _loop(directory)
 
 
 try:
