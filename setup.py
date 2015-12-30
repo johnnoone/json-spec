a4bdd20b54b0b6cf2545924788f18ecbec8da295
Richard Downe <richard@casetext.com>
2015-12-30 12:52:23 -0800

    Added zip_safe=False flag so spec provider works.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
+    zip_safe=False,
     keywords=[
         'json',
         'utilitaries',
